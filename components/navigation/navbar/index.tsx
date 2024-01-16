a29f623ba72e92d9c64c50b556e1b47d7e326e05
bagusyanuar <bagus.yanuar613@gmail.com>
2024-01-16 21:28:36 +0700

    a

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 
 import Image from 'next/image'
+import styled from 'styled-components'
+
+const NavLink = styled.li`
+  padding: 0.3rem 1.25rem;
+  cursor: pointer;
+  border-bottom: 0.175rem solid transparent;
+  &:hover {
+    border-bottom: 0.175rem solid blue;
+  }
+`
 
 function Index() {
   return (
     <div className='flex items-center py-3 px-20 justify-between'>
-        <Image src="/assets/images/brand.png" height={40} width={70} alt='brand-logo' />
-        <div className='flex'>
-            <ul className='flex'>
-                <li>Chek</li>
-            </ul>
-        </div>
+      <Image src="/assets/images/brand.png" height={40} width={70} alt='brand-logo' />
+      <div className='flex'>
+        <ul className='flex'>
+          <NavLink>
+            <a href='#'>Home</a>
+          </NavLink>
+          <NavLink>Portofolio</NavLink>
+        </ul>
+      </div>
     </div>
   )
 }
